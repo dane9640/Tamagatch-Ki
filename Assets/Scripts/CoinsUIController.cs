0fe9b0cde53b0ddd6335caaf609386e04cc73b1f
dane9640 <gandalf12298@gmail.com>
2020-08-16 17:00:19 -0400

    v.0.0.2h
    Fixed Bugs where pet had rigid body
    Fixed Sleep Animation Bug

--- Assets/Scripts/CoinsUIController.cs
+++ Assets/Scripts/CoinsUIController.cs
@@ -19,6 +19,6 @@ public class CoinsUIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       StatText.text = "Coins: " + (this.inventory.GetTotalCoins().ToString());
+       StatText.text = (this.inventory.GetTotalCoins().ToString());
     }
 }
